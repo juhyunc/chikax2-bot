15fc0df2c653964ea9370021462a2235f35036a0
Juhyun Chung <79008321+juhyunc@users.noreply.github.com>
2021-07-07 21:48:35 +0900

    chikax2-bot_210702
    Modified version of chikax2-bot
    Version name YYMMDD

---
+++ README.md
@@ -0,0 +1,4 @@
+# chikax2-bot
+Twitter @chikax2_bot
+This bot tweets about useful dental information.
+Created by Juhyun Chung, DDS
---
+++ tweet_info.py
@@ -0,0 +1,15 @@
+import random
+import config
+import tweet
+
+
+def main():
+    filename=open('text_info.txt','r')
+    text=filename.readlines()
+    filename.close()
+    
+    count=random.randint(0,len(text)-1)
+    tweet.post(text[count])
+
+if __name__ == "__main__":
+    main()
